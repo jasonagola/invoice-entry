1fc8065f0398c5a7e0da8ee923e669744c32548d
jasonagola <jason.agola@gmail.com>
2023-01-03 14:38:48 -0500

    env

--- Backend/databaseConfig.js
+++ Backend/databaseConfig.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql')
+require('dotenv').config()
 
 const db = mysql.createPool({
     connectionLimit: 10,
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_DATABASE
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
 
 db.on('error', function (err) {
